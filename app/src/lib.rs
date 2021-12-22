9c82931726632063b0249a7bdda6451f68148282
nrskt <norisuke_takafuji@caddi.jp>
2021-12-22 23:08:40 +0900

    feat(app): implement main function

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,1 +1,18 @@
-pub struct App {}
+use adaptor::HashMapDb;
+use application::BacklogApplication;
+use ports::ProvideBacklogRepository;
+
+#[derive(Debug, Clone, Default)]
+pub struct App {
+    db: HashMapDb,
+}
+
+impl BacklogApplication for App {}
+
+impl ProvideBacklogRepository for App {
+    type Repository = HashMapDb;
+
+    fn provide(&self) -> &Self::Repository {
+        &self.db
+    }
+}
--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use app::App;
+use application::{AddItemCmd, BacklogApplication};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let app = App::default();
+
+    let backlog = app.add_item(Box::new(Cmd)).await?;
+    println!("{:?}", backlog);
+    Ok(())
+}
+
+struct Cmd;
+
+impl AddItemCmd for Cmd {
+    fn item_type(&self) -> application::ItemType {
+        application::ItemType::Story
+    }
+
+    fn title(&self) -> &str {
+        "dummy title"
+    }
 }
